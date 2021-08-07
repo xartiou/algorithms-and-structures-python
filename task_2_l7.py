b5d4fa4208c5e4c66266d0587e9e3993869f0ca6
Xartiou <76091221+xartiou@users.noreply.github.com>
2021-08-07 13:03:52 +0300

    Create task_2_l7.py

---
+++ task_2_l7.py
@@ -0,0 +1,44 @@
+# 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+# заданный случайными числами на промежутке [0; 50).
+# Выведите на экран исходный и отсортированный массивы.
+
+
+import random
+
+numbers = [random.randint(0, 50) for _ in range(10)]
+
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        center = len(nums) // 2
+        left = nums[:center]
+        right = nums[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+        return nums
+
+
+print('Исходный массив:', numbers)
+print('Сортировка слиянием:', merge_sort(numbers[:]))
